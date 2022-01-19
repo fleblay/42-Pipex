d88441f9f1ce86eeacf98367cce1c1076b7b28fa
Frederic LE BLAY <fle-blay@e2r2p21.clusters.42paris.fr>
2022-01-19 14:44:25 +0100

    improved a bit algo push_swap

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,11 +6,22 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 12:42:27 by fle-blay          #+#    #+#             */
-/*   Updated: 2022/01/19 12:42:54 by fle-blay         ###   ########.fr       */
+/*   Updated: 2022/01/19 14:34:26 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+
+#include <unistd.h>
+
+typedef struct s_data {
+	char	**path;
+	char	***cmd;
+	int		**pipefd;
+	t_pid	*child;
+	int		error;
+}			t_data;
+
 
 #endif
