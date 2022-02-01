4959e15f21ca69e3b7956e113927c6af7da14e9b
Frederic LE BLAY <fle-blay@e2r1p3.clusters.42paris.fr>
2022-02-01 19:36:34 +0100

    wip lsof -p

--- srcs/fork.c
+++ srcs/fork.c
@@ -6,7 +6,7 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 16:09:57 by fle-blay          #+#    #+#             */
-/*   Updated: 2022/02/01 18:24:43 by fle-blay         ###   ########.fr       */
+/*   Updated: 2022/02/01 19:24:24 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 static void	treat_here_doc(t_data *data)
 {
@@ -61,9 +62,8 @@ static void	treat_child(int i, t_data *data)
 	/*
 	ft_putstr_fd("ici", 2);
 	ft_putnbr_fd(i, 2);
-	s_close(data->pipefd[i + 1][0], data);
 	*/
-	
+
 	try_cmd(i + 2, data);
 	if (data->cmds_type[i + 2] == -1)
 		error_cmd(data, "pipex: command not found: ", data->av[i + 2], 127);
@@ -92,7 +92,6 @@ static void	treat_last(int i, t_data *data)
 
 //on enleve le wait avant le i++ pour ne plus timeout sur /dev/urandom | head -1
 
-#include <sys/wait.h>
 
 void	make_fork(t_data *data)
 {
