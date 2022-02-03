328caff06daa262d5a1dc44f17dd202a5c3b0887
Frederic LE BLAY <fle-blay@e2r3p1.clusters.42paris.fr>
2022-02-03 10:04:12 +0100

    Fonctional Pipex : AT LEAST

--- srcs/fork2.c
+++ srcs/fork2.c
@@ -6,7 +6,7 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 19:07:54 by fle-blay          #+#    #+#             */
-/*   Updated: 2022/02/02 19:33:02 by fle-blay         ###   ########.fr       */
+/*   Updated: 2022/02/03 10:01:56 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <stdio.h>
 
 static void	treat_here_doc(t_data *data)
 {
@@ -43,6 +42,18 @@ static void	treat_here_doc(t_data *data)
 	s_close(data->fd1, data);
 }
 
+static void	treat_last(int i, t_data *data)
+{
+	if (data->hd)
+		data->fd2 = s_open(data->av[i + 3],
+				O_CREAT | O_WRONLY | O_APPEND, S_IRWXU, data);
+	else
+		data->fd2 = s_open(data->av[i + 3],
+				O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU, data);
+	s_dup2(data->fd2, 1, data);
+	s_close(data->fd2, data);
+}
+
 static void	treat_child(int i, t_data *data)
 {
 	if (i == 0 && !data->hd)
@@ -63,20 +74,9 @@ static void	treat_child(int i, t_data *data)
 		s_dup2(data->pipefd[i][1], 1, data);
 		s_close(data->pipefd[i][1], data);
 	}
-	else
-	{
-		if (data->hd)
-			data->fd2 = s_open(data->av[i + 3],
-					O_CREAT | O_WRONLY | O_APPEND, S_IRWXU, data);
-		else
-			data->fd2 = s_open(data->av[i + 3],
-					O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU, data);
-		s_dup2(data->fd2, 1, data);
-		s_close(data->fd2, data);
-	}
+	else if (i == data->ac - 4)
+		treat_last(i, data);
 	try_cmd(i + 2, data);
-	fprintf(stderr, "\x1b[31mENFANT %d just avant execve child \n\x1b[0m", i);
-	print_pipes(data);
 	exit(execve(data->cmds[i + 2][0], data->cmds[i + 2], data->env));
 }
 
@@ -98,13 +98,13 @@ void	make_fork(t_data *data)
 		}
 		else
 		{
+			s_close(0, data);
 			waitpid(data->child[i], &(data->status), 0);
 			if (WIFEXITED(data->status) && WEXITSTATUS(data->status) != 0)
 				data->status = WEXITSTATUS(data->status);
-			while (wait(NULL) > 0);
+			while (wait(NULL) > 0)
+				;
 		}
-		fprintf(stderr, "\x1b[31mPARENT %d just avant i++\n\x1b[0m", i);
-		print_pipes(data);
 		i++;
 	}
 }
